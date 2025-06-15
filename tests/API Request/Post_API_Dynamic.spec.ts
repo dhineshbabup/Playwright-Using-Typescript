3726d5148c3639e615db7e756f16a255d0587c5b
dhineshbabu <dhineshbabup23@gmail.com>
2025-06-15 12:40:20 +0530

    Post API using Dynamic files

--- tests/API Request/Post_API_Dynamic.spec.ts
+++ tests/API Request/Post_API_Dynamic.spec.ts
@@ -1,16 +1,20 @@
 import { test, expect } from "@playwright/test";
-import postAPIStatic from "../../test-data/api_request/post_api_request.json";
 import postAPIDynamic from "../../test-data/api_request/Dynamic_Post_api_request.json";
 import { formatAPIRequest } from "../../src/utils/APIHelper";
 import path from "path";
 import fs from "fs";
 
-test.use({
-  baseURL: process.env.BASE_API_URL,
-});
-
-test("Post request using static files", async ({ request }) => {
-  const response = await request.post(`/booking`, { data: postAPIStatic });
+test("Post request using dynamic files", async ({ request }) => {
+  const filePath = path.join(
+    __dirname,
+    "../../test-data/api_request/Dynamic_Post_api_request.json"
+  );
+  const jsonTemplate = fs.readFileSync(filePath, "utf-8");
+  const values = ["Dhinesh", "P", 2000];
+  const APIBody = await formatAPIRequest(jsonTemplate, values);
+  const response = await request.post(`/booking`, {
+    data: JSON.parse(APIBody),
+  });
 
   const resJSON = await response.json();
   console.log(JSON.stringify(resJSON, null, 2));
@@ -24,5 +28,5 @@ test("Post request using static files", async ({ request }) => {
   await expect(resJSON.booking).toHaveProperty("firstname");
 
   await expect(resJSON.bookingid).toBeGreaterThan(0);
-  await expect(resJSON.booking.firstname).toBe("Dhinesh");
+  await expect(resJSON.booking.firstname).toBe(values[0]);
 });
