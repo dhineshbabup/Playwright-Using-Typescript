f8877c4d53fc3aabc4d8eaa500eb4dec288b80a1
dhineshbabu <dhineshbabup23@gmail.com>
2025-06-15 13:04:17 +0530

    changes in locators

--- tests/Record/RecordAtCursor_Test.spec.ts
+++ tests/Record/RecordAtCursor_Test.spec.ts
@@ -6,7 +6,7 @@ test('test', async ({ page }) => {
   await page.getByRole('combobox', { name: 'Search' }).fill('testers talk');
   await page.getByRole('combobox', { name: 'Search' }).press('Enter');
   await page.locator('#avatar-section').getByRole('link').click();
-  await page.getByText('Playlists', { exact: true }).click();
-  await expect(page.getByRole('link', { name: 'Learn Complete Playwright in' })).toBeVisible();
+  await page.locator('#tabsContent').getByText('Playlists').click();
   await expect(page.getByRole('link', { name: 'Learn Complete Playwright with TypeScript in 20 Hours | Learn Playwright From' })).toBeVisible();
+  await expect(page.getByRole('link', { name: 'Playwright Complete Series -' })).toBeVisible();
 });
