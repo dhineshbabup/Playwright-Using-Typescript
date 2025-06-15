ae8c92304c70329d287f89fc172b463c7e765fd8
dhineshbabu <dhineshbabup23@gmail.com>
2025-06-15 12:58:21 +0530

    Post API with typesafety

---
+++ src/interface/Booking_API.ts
@@ -0,0 +1,12 @@
+interface BookingAPI {
+  firstname: string;
+  lastname: string;
+  totalprice: number;
+  depositpaid: boolean;
+  additionalneeds: string;
+  bookingdates: BookingDates;
+}
+interface BookingDates {
+  checkin: string;
+  checkout: string;
+}
